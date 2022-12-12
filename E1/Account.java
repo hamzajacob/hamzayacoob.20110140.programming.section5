9f807bb43e6f36df46a43bc1f87c4e96a03758ef
hamzajacob <20110140@htu.edu.jo>
2022-12-12 10:06:16 +0300

    Exercise 1

---
+++ E1/Account.java
@@ -0,0 +1,26 @@
+package lecture7;
+
+public class Account {
+	
+	int accountNo;
+	String name;
+	float amount;
+	
+	
+	public void accountNo()
+	{
+		System.out.println("20110140");
+	}
+	
+	public void name()
+	{
+		System.out.println("..Hamza..");
+	}
+	public void amount()
+	{
+		System.out.println("2039.323");
+	}
+	
+	
+
+}
---
+++ E1/Data.java
@@ -0,0 +1,24 @@
+package lecture7;
+
+public class Data {
+
+	
+	public class void main (String [] args)
+	{
+		Account Data = new Account;
+		
+		Data.accountNo();
+		Data.name();
+		Data.amount();
+		
+		System.out.println("accountNo :");
+		System.out.println("name :");
+		System.out.println("amount :");
+		
+		
+	}
+	
+	
+	
+	
+}
